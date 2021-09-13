e8bfbea1de8670e398590a7d11516f0e66f17e5f
PavelBlend <stalkermodkytia@yandex.ru>
2021-09-14 00:18:04 +0300

    blender 2.8+ port

--- uv_image_scroller.py
+++ uv_image_scroller.py
@@ -1,132 +1,156 @@
-
 bl_info = {
     'name': 'Image Scroller',
-    'description': 'Assigns hotkeys for scrolling images in UV/Image Editor',
+    'description': 'Assigns hotkeys for scrolling images in Image Editor',
     'author': 'Pavel_Blend',
-    'version': (0, 0, 1),
-    'blender': (2, 78, 0),
-    'location': 'UV/Image Editor > Page Up, Page Down, '
-                'Shift + Page Up, Shift + Page Down',
+    'version': (0, 1, 0),
+    'blender': (2, 80, 0),
+    'location':
+        'Image Editor > Page Up, Page Down, '
+        'Shift + Page Up, Shift + Page Down',
     'category': 'UV',
-    }
+}
 
 
 import bpy
 
 
-def set_last_or_first_image(context, image_index):
+def set_image_by_index(context, image_index):
     images_count = len(bpy.data.images)
     if images_count > 0:
         active_image = context.area.spaces[0].image
         if active_image:
-            image = bpy.data.images[image_index]
-            context.area.spaces[0].image = image
+            bpy_image = bpy.data.images[image_index]
+            context.area.spaces[0].image = bpy_image
 
 
-class ImageScroller(bpy.types.Operator):
+def get_image_index(context):
+    images_count = len(bpy.data.images)
+    if images_count > 0:
+        active_image = context.area.spaces[0].image
+        if active_image:
+            for image_index, image in enumerate(bpy.data.images):
+                if image.name == active_image.name:
+                    return image_index, images_count
+    return None, None
+
+
+class BaseOperator(bpy.types.Operator):
     @classmethod
     def poll(self, context):
-        area = context.area.type == 'IMAGE_EDITOR'
-        ob = bpy.context.object
-        if ob:
-            mode = ob.mode == 'OBJECT'
-        else:
-            mode = True
-        return area and mode
+        is_image_editor = context.area.type == 'IMAGE_EDITOR'
+        return is_image_editor
 
 
-class ImageScroller_NextImage(ImageScroller):
+class IMGSCROLL_OT_next_image(BaseOperator):
     bl_idname = 'uv.next_image'
-    bl_label = 'Next Image'
+    bl_label = 'Image Scroller: Next Image'
 
     def execute(self, context):
-        images_count = len(bpy.data.images)
-        if images_count > 0:
-            active_image = context.area.spaces[0].image
-            if active_image:
-                for image_index, image in enumerate(bpy.data.images):
-                    if image.name == active_image.name:
-                        break
-                image_index += 1
-                if image_index < images_count:
-                    next_image = bpy.data.images[image_index]
-                    context.area.spaces[0].image = next_image
+        image_index, images_count = get_image_index(context)
+        if not (image_index is None):
+            image_index += 1
+            if image_index < images_count:
+                next_image = bpy.data.images[image_index]
+                context.area.spaces[0].image = next_image
         return {'FINISHED'}
 
 
-class ImageScroller_PreviousImage(ImageScroller):
-    bl_idname = 'uv.previous_image'
-    bl_label = 'Previous Image'
+class IMGSCROLL_OT_prev_image(BaseOperator):
+    bl_idname = 'uv.prev_image'
+    bl_label = 'Image Scroller: Previous Image'
 
     def execute(self, context):
-        images_count = len(bpy.data.images)
-        if images_count > 0:
-            active_image = context.area.spaces[0].image
-            if active_image:
-                for image_index, image in enumerate(bpy.data.images):
-                    if image.name == active_image.name:
-                        break
-                image_index -= 1
-                if image_index >= 0:
-                    previous_image = bpy.data.images[image_index]
-                    context.area.spaces[0].image = previous_image
+        image_index, images_count = get_image_index(context)
+        if not (image_index is None):
+            image_index -= 1
+            if image_index >= 0:
+                prev_image = bpy.data.images[image_index]
+                context.area.spaces[0].image = prev_image
         return {'FINISHED'}
 
 
-class ImageScroller_LastImage(ImageScroller):
+class IMGSCROLL_OT_last_image(BaseOperator):
     bl_idname = 'uv.last_image'
-    bl_label = 'Last Image'
+    bl_label = 'Image Scroller: Last Image'
 
     def execute(self, context):
-        set_last_or_first_image(context, -1)
+        set_image_by_index(context, -1)
         return {'FINISHED'}
 
 
-class ImageScroller_FirtsImage(ImageScroller):
+class IMGSCROLL_OT_first_image(BaseOperator):
     bl_idname = 'uv.first_image'
-    bl_label = 'First Image'
+    bl_label = 'Image Scroller: First Image'
 
     def execute(self, context):
-        set_last_or_first_image(context, 0)
+        set_image_by_index(context, 0)
         return {'FINISHED'}
 
 
 addon_keymaps = []
+operators = (
+    IMGSCROLL_OT_next_image,
+    IMGSCROLL_OT_prev_image,
+    IMGSCROLL_OT_last_image,
+    IMGSCROLL_OT_first_image
+)
+
+
+def add_keymap_item(keymaps, operator, key, shift=False):
+    keymap_item = keymaps.keymap_items.new(
+        operator,
+        key,
+        'PRESS',
+        shift=shift,
+        repeat=True
+    )
+    addon_keymaps.append((keymaps, keymap_item))
 
 
 def register():
-    bpy.utils.register_class(ImageScroller_NextImage)
-    bpy.utils.register_class(ImageScroller_PreviousImage)
-    bpy.utils.register_class(ImageScroller_LastImage)
-    bpy.utils.register_class(ImageScroller_FirtsImage)
-
-    wm = bpy.context.window_manager
-    if wm.keyconfigs.addon:
-        km = wm.keyconfigs.addon.keymaps.new(
+    for operator in operators:
+        bpy.utils.register_class(operator)
+    window_manager = bpy.context.window_manager
+    key_config = window_manager.keyconfigs.addon
+    if key_config:
+        keymaps = key_config.keymaps.new(
             name='Image',
             space_type='IMAGE_EDITOR'
-            )
-        kmi = km.keymap_items.new('uv.next_image', 'PAGE_DOWN', 'PRESS')
-        addon_keymaps.append((km, kmi))
-        kmi = km.keymap_items.new('uv.previous_image', 'PAGE_UP', 'PRESS')
-        addon_keymaps.append((km, kmi))
-        kmi = km.keymap_items.new(
-            'uv.last_image', 'PAGE_DOWN', 'PRESS', shift=True)
-        addon_keymaps.append((km, kmi))
-        kmi = km.keymap_items.new(
-            'uv.first_image', 'PAGE_UP', 'PRESS', shift=True)
-        addon_keymaps.append((km, kmi))
+        )
+        # next
+        keymap_item = add_keymap_item(
+            keymaps,
+            IMGSCROLL_OT_next_image.bl_idname,
+            'PAGE_DOWN'
+        )
+        # prev
+        keymap_item = add_keymap_item(
+            keymaps,
+            IMGSCROLL_OT_prev_image.bl_idname,
+            'PAGE_UP'
+        )
+        # last
+        keymap_item = add_keymap_item(
+            keymaps,
+            IMGSCROLL_OT_last_image.bl_idname,
+            'PAGE_DOWN',
+            shift=True
+        )
+        # first
+        keymap_item = add_keymap_item(
+            keymaps,
+            IMGSCROLL_OT_first_image.bl_idname,
+            'PAGE_UP',
+            shift=True
+        )
 
 
 def unregister():
-    bpy.utils.unregister_class(ImageScroller_NextImage)
-    bpy.utils.unregister_class(ImageScroller_PreviousImage)
-    bpy.utils.unregister_class(ImageScroller_LastImage)
-    bpy.utils.unregister_class(ImageScroller_FirtsImage)
-
-    wm = bpy.context.window_manager
-    kc = wm.keyconfigs.addon
-    if kc:
-        for km, kmi in addon_keymaps:
-            km.keymap_items.remove(kmi)
+    window_manager = bpy.context.window_manager
+    key_config = window_manager.keyconfigs.addon
+    if key_config:
+        for keymaps, keymap_item in addon_keymaps:
+            keymaps.keymap_items.remove(keymap_item)
     addon_keymaps.clear()
+    for operator in reversed(operators):
+        bpy.utils.unregister_class(operator)
