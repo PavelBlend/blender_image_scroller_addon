31e9b49b045433fa98fa1a959bea8df5ba7ffdaa
Pavel_Blend <stalkermodkytia@yandex.ru>
2022-08-23 22:16:40 +0300

    refactor (v0.2.0)

--- uv_image_scroller.py
+++ uv_image_scroller.py
@@ -2,8 +2,8 @@ bl_info = {
     'name': 'Image Scroller',
     'description': 'Assigns hotkeys for scrolling images in Image Editor',
     'author': 'Pavel_Blend',
-    'version': (0, 1, 1),
-    'blender': (2, 80, 0),
+    'version': (0, 2, 0),
+    'blender': (3, 2, 0),
     'location':
         'Image Editor > Page Up, Page Down, '
         'Shift + Page Up, Shift + Page Down',
@@ -16,89 +16,82 @@ import bpy
 
 def set_image_by_index(context, image_index):
     images_count = len(bpy.data.images)
-    if images_count > 0:
+    if not images_count:
+        return
+    if image_index < images_count:
         bpy_image = bpy.data.images[image_index]
         context.area.spaces[0].image = bpy_image
 
 
 def get_image_index(context):
     images_count = len(bpy.data.images)
-    if images_count > 0:
-        active_image = context.area.spaces[0].image
-        if active_image:
-            for image_index, image in enumerate(bpy.data.images):
-                if image.name == active_image.name:
-                    return image_index, images_count
-    return None, None
+    if not images_count:
+        return
+    active_image = context.area.spaces[0].image
+    if not active_image:
+        return
+    for image_index, image in enumerate(bpy.data.images):
+        if image is active_image:
+            return image_index
 
 
-class BaseOperator(bpy.types.Operator):
+def get_key_config():
+    window_manager = bpy.context.window_manager
+    key_config = window_manager.keyconfigs.addon
+    return key_config
+
+
+MODE_NEXT = 'NEXT'
+MODE_PREV = 'PREV'
+MODE_FIRST = 'FIRST'
+MODE_LAST = 'LAST'
+
+keys = ('PAGE_DOWN', 'PAGE_UP', 'PAGE_DOWN', 'PAGE_UP')
+shifts = (False, False, True, True)
+modes = (MODE_NEXT, MODE_PREV, MODE_LAST, MODE_FIRST)
+
+
+class IMGSCROLL_OT_scroll_image(bpy.types.Operator):
+    bl_idname = 'uv.scroll_image'
+    bl_label = 'Scroll Image'
+
+    mode: bpy.props.EnumProperty(
+        name='Mode',
+        default=MODE_NEXT,
+        items=(
+            (MODE_NEXT, '', ''),
+            (MODE_PREV, '', ''),
+            (MODE_FIRST, '', ''),
+            (MODE_LAST, '', '')
+        )
+    )
+
     @classmethod
     def poll(self, context):
-        is_image_editor = context.area.type == 'IMAGE_EDITOR'
-        return is_image_editor
-
-
-class IMGSCROLL_OT_next_image(BaseOperator):
-    bl_idname = 'uv.next_image'
-    bl_label = 'Image Scroller: Next Image'
+        return context.area.type == 'IMAGE_EDITOR'
 
     def execute(self, context):
-        image_index, images_count = get_image_index(context)
-        if not (image_index is None):
-            image_index += 1
-            if image_index < images_count:
-                next_image = bpy.data.images[image_index]
-                context.area.spaces[0].image = next_image
-        else:
-            bpy.ops.uv.first_image()
-        return {'FINISHED'}
-
-
-class IMGSCROLL_OT_prev_image(BaseOperator):
-    bl_idname = 'uv.prev_image'
-    bl_label = 'Image Scroller: Previous Image'
-
-    def execute(self, context):
-        image_index, images_count = get_image_index(context)
-        if not (image_index is None):
-            image_index -= 1
-            if image_index >= 0:
-                prev_image = bpy.data.images[image_index]
-                context.area.spaces[0].image = prev_image
-        else:
-            bpy.ops.uv.first_image()
-        return {'FINISHED'}
-
-
-class IMGSCROLL_OT_last_image(BaseOperator):
-    bl_idname = 'uv.last_image'
-    bl_label = 'Image Scroller: Last Image'
-
-    def execute(self, context):
-        set_image_by_index(context, -1)
-        return {'FINISHED'}
-
-
-class IMGSCROLL_OT_first_image(BaseOperator):
-    bl_idname = 'uv.first_image'
-    bl_label = 'Image Scroller: First Image'
-
-    def execute(self, context):
-        set_image_by_index(context, 0)
+        if self.mode in (MODE_NEXT, MODE_PREV):
+            image_index = get_image_index(context)
+            if image_index is None:
+                image_index = 0
+            else:
+                if self.mode == MODE_NEXT:
+                    image_index += 1
+                else:
+                    image_index -= 1
+        elif self.mode == MODE_FIRST:
+            image_index = 0
+        elif self.mode == MODE_LAST:
+            image_index = -1
+        set_image_by_index(context, image_index)
         return {'FINISHED'}
 
 
 addon_keymaps = []
-operators = (
-    IMGSCROLL_OT_next_image,
-    IMGSCROLL_OT_prev_image,
-    IMGSCROLL_OT_last_image,
-    IMGSCROLL_OT_first_image
-)
 
 
-def add_keymap_item(keymaps, operator, key, shift=False):
+def add_keymap_item(keymaps, operator, key, mode, shift):
     keymap_item = keymaps.keymap_items.new(
         operator,
         key,
@@ -106,53 +99,32 @@ def add_keymap_item(keymaps, operator, key, shift=False):
         shift=shift,
         repeat=True
     )
+    keymap_item.properties.mode = mode
     addon_keymaps.append((keymaps, keymap_item))
 
 
 def register():
-    for operator in operators:
-        bpy.utils.register_class(operator)
-    window_manager = bpy.context.window_manager
-    key_config = window_manager.keyconfigs.addon
+    bpy.utils.register_class(IMGSCROLL_OT_scroll_image)
+    key_config = get_key_config()
     if key_config:
         keymaps = key_config.keymaps.new(
             name='Image',
             space_type='IMAGE_EDITOR'
         )
-        # next
-        keymap_item = add_keymap_item(
-            keymaps,
-            IMGSCROLL_OT_next_image.bl_idname,
-            'PAGE_DOWN'
-        )
-        # prev
-        keymap_item = add_keymap_item(
-            keymaps,
-            IMGSCROLL_OT_prev_image.bl_idname,
-            'PAGE_UP'
-        )
-        # last
-        keymap_item = add_keymap_item(
-            keymaps,
-            IMGSCROLL_OT_last_image.bl_idname,
-            'PAGE_DOWN',
-            shift=True
-        )
-        # first
-        keymap_item = add_keymap_item(
-            keymaps,
-            IMGSCROLL_OT_first_image.bl_idname,
-            'PAGE_UP',
-            shift=True
-        )
+        for key, shift, mode in zip(keys, shifts, modes):
+            keymap_item = add_keymap_item(
+                keymaps,
+                IMGSCROLL_OT_scroll_image.bl_idname,
+                key,
+                mode,
+                shift
+            )
 
 
 def unregister():
-    window_manager = bpy.context.window_manager
-    key_config = window_manager.keyconfigs.addon
+    key_config = get_key_config()
     if key_config:
         for keymaps, keymap_item in addon_keymaps:
             keymaps.keymap_items.remove(keymap_item)
     addon_keymaps.clear()
-    for operator in reversed(operators):
-        bpy.utils.unregister_class(operator)
+    bpy.utils.unregister_class(IMGSCROLL_OT_scroll_image)
